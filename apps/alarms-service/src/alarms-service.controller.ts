f356cdf1e2cd0bc92232deca5c370d9bb1761543
KalPal <reznichenkomykola1@gmail.com>
2024-09-04 12:57:24 +0300

    14 Alarm Classifier and Notifications Services

--- apps/alarms-service/src/alarms-service.controller.ts
+++ apps/alarms-service/src/alarms-service.controller.ts
@@ -1,15 +1,35 @@
-import { Body, Controller, Get, Logger } from '@nestjs/common';
-import { EventPattern } from '@nestjs/microservices';
+import { Body, Controller, Get, Inject, Logger } from '@nestjs/common';
+import { ClientProxy, EventPattern } from '@nestjs/microservices';
+import { MESSAGE_BROKER } from './constants';
+import { lastValueFrom } from 'rxjs';
 
 @Controller()
 export class AlarmsServiceController {
   logger = new Logger();
 
+  constructor(
+    @Inject(MESSAGE_BROKER) private readonly messageBroker: ClientProxy,
+  ) {}
+
   @EventPattern('alarms.create')
   @Get()
-  getHello(@Body() data: unknown) {
+  async getHello(@Body() data: { name: string }) {
     this.logger.debug(
       `Alarms.create event received with data: ${JSON.stringify(data)}`,
     );
+
+    const alarmClassification = await lastValueFrom(
+      this.messageBroker.send('alarm.classify', data),
+    );
+    this.logger.debug(
+      `Alarm "${data.name}" classified as ${alarmClassification.category}`,
+    );
+
+    const notify = this.messageBroker.emit(
+      'notification.send',
+      alarmClassification,
+    );
+    await lastValueFrom(notify);
+    this.logger.debug(`Dispatched "notification.send" event`);
   }
 }
--- apps/alarms-service/src/alarms-service.module.ts
+++ apps/alarms-service/src/alarms-service.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AlarmsServiceController } from './alarms-service.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { MESSAGE_BROKER } from './constants';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: MESSAGE_BROKER,
+        transport: Transport.NATS,
+        options: {
+          servers: process.env.NATS_URL,
+        },
+      },
+    ]),
+  ],
   controllers: [AlarmsServiceController],
 })
 export class AlarmsServiceModule {}
